e92a7d538fd5ce8cf44a17b1f583a275645d6b1b
qslim <mqyang1s@163.com>
2019-05-22 14:17:23 +0800

    clean

--- backtrack_searcher.py
+++ backtrack_searcher.py
@@ -4,22 +4,15 @@ from ac_enforcer import ACEnforcer
 from build_matrix import parser
 import time
 
+
 class BackTrackSearcher:
     def __init__(self, rel_, N, D):
-        # self.vars = [[1, 2, 3], [1, 3, 4], [3, 6, 7]]
         self.acer = ACEnforcer(rel_, N, D)
-        # self.vars_backup = []
         self.D = D
         self.N = N
         self.var_mask = np.ones([D, 1])
         self.count = 0
         self.answer = None
-
-    # def pre_processing(self, vars_):
-    #     if self.acer.ac_enforcer(vars_):
-    #         print("network inconsistency.")
-    #         return False
-    #     return True
 
     def assignment(self, var_index, val_index, vars_pre):
         self.count += 1
@@ -29,11 +22,6 @@ class BackTrackSearcher:
             var[p] = 0
         var[val_index] = 1
         return vars_re
-
-    # @staticmethod
-    # def arrive_answer(vars_):
-    #     print("got an answer...")
-    #     print(vars_)
 
     def var_heuristics(self, vars_):
         dom = np.matmul(np.squeeze(vars_), self.var_mask).squeeze()
@@ -47,21 +35,16 @@ class BackTrackSearcher:
     def dfs(self, level, vars_pre):
         # print(level)
         if level == self.N:
-            print("NNNNNNNN")
             self.answer = vars_pre
             return True
 
-        # tmp = vars_pre.copy()
         vars_pre = self.acer.ac_enforcer(vars_pre)
         if vars_pre is None:
-            # print((tmp == vars_pre).all())
             return False
-        # print((tmp == vars_pre).all())
 
         var_index = self.var_heuristics(vars_pre)
 
         if var_index == -1:
-            print("IIIIIIII")
             self.answer = vars_pre
             return True
 
@@ -69,11 +52,7 @@ class BackTrackSearcher:
         for i in range(self.D):
             if var[i] == 0:
                 continue
-
             vars_re = self.assignment(var_index, i, vars_pre)
-            # if self.acer.ac_enforcer(vars_re):
-            #     continue
-
             if self.dfs(level + 1, vars_re):
                 return True
 
@@ -81,13 +60,7 @@ class BackTrackSearcher:
 
 
 N, D, vars_map, cons_map = parser("/home/ymq/csp_benchmark/run_dir/rand-2-26/rand-26-26-325-155-53021_ext.xml")
-print("DONE LOADING")
-
-# print(D)
-# print(vars_map.shape)
-# print(cons_map.shape)
-# print(cons_map[0][2])
-# print(vars_map)
+print("cons shape:", cons_map.shape, " vars shape:", vars_map.shape)
 
 
 bs = BackTrackSearcher(cons_map, N, D)
@@ -102,7 +75,3 @@ else:
 print("Lasts: ", time.time() - ticks)
 
 print("Node =", bs.count)
-
-# if bs.pre_processing(vars_map):
-#     bs.dfs(0, vars_map)
-    # pass
