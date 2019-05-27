76660099e2d4c051c684aeb764f2749a5229c86f
qslim <mqyang1s@163.com>
2019-05-27 22:13:27 +0800

    sac

--- ac_parallel.py
+++ ac_parallel.py
@@ -8,30 +8,36 @@ class ACParallel:
         self.cons_map = cons_map
         self._1n_mask1 = torch.ones((1, N)).to(device)
         self.n1_mask0 = torch.zeros((N, 1)).to(device)
+        self.nnndd_mask1 = torch.ones((N, N, N, D, D)).to(device)
         self.nndd_mask1 = torch.ones((N, N, D, D)).to(device)
-        self.ndd_mask1 = torch.ones((N, D, D)).to(device)
-        self.ndd_mask0 = torch.zeros((N, D, D)).to(device)
+        self.nndd_mask0 = torch.zeros((N, N, D, D)).to(device)
         # self.count = 0
+        self.nndt1_mask1 = torch.ones((N, N, D, 1)).to(device)
+        self.nnd_mask1 = torch.ones((N, N, D)).to(device)
+        self.nd_mask1 = torch.ones((N, D)).to(device)
+        self.nd_mask0 = torch.zeros((N, D)).to(device)
 
     def ac_enforcer(self, vars_map):
         # print(vars_map.type())
-        vars_map_pre = self.ndd_mask0
-        while torch.equal(vars_map, vars_map_pre) is False:
+        n1d = vars_map.unsqueeze(1)
+        nndd = torch.matmul(self.nndt1_mask1, n1d)
+        vars_map_pre = self.nndd_mask0
+        while torch.equal(nndd, vars_map_pre) is False:
             # print("~~~~~~~~~~~~~~~~~~~~~~~~")
 
-            vars_map_pre = vars_map
+            vars_map_pre = nndd
+            nn1dd = nndd.unsqueeze(1)
+            nnndd = torch.matmul(nn1dd, self.cons_map)
+            nnndd_reduce = torch.where(nnndd > 1, self.nnndd_mask1, nnndd)
+            nndnd = nnndd_reduce.transpose(2, 3)
+            nndd_ = torch.matmul(self._1n_mask1, nndnd).squeeze()
+            nndd = torch.where(nndd_ == self.N, self.nndd_mask1, self.nndd_mask0)
 
-            nndd = torch.matmul(vars_map, self.cons_map)
-
-            nndd_reduce = torch.where(nndd > 1, self.nndd_mask1, nndd)
-
-            ndnd = nndd_reduce.transpose(1, 2)
-
-            ndd = torch.matmul(self._1n_mask1, ndnd).squeeze()
-
-            vars_map = torch.where(ndd == self.N, self.ndd_mask1, self.ndd_mask0)
-
-            if (vars_map.sum((1, 2)) == self.n1_mask0).any():
+            nnd = nndd.sum(3)
+            nnd = torch.where(nnd > 1, self.nnd_mask1, nnd)
+            nd = nnd.sum(1)
+            vars_map = torch.where(nd == self.N, self.nd_mask1, self.nd_mask0)
+            if (vars_map.sum(1) == self.n1_mask0).any():
                 return None
 
         return vars_map
