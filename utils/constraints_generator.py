acfebdbf2a08385d875c548829d69b48eba54701
qslim <mqyang1s@163.com>
2020-03-10 22:46:29 +0800

    update

--- utils/constraints_generator.py
+++ utils/constraints_generator.py
@@ -63,7 +63,7 @@ bm_name = '../csp-benchmark/conmap-' \
           + '-' + str(max_domain) \
           + '-' + str(con_density) \
           + '-' + str(differenty) \
-          + '-' + str(time.time()) \
+          + '-' + str(int(time.time())) \
           + '.dump'
 
 constraints_map = constraints_generator(max_domain, num_variables, con_density, differenty)
