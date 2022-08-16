40c347bc8ce97e5f4d56023fd6f3b17fafacbc64
qslim <mqyang1s@163.com>
2022-08-16 17:35:04 +0800

    clean name

--- matrix_main.py
+++ matrix_main.py
@@ -1,50 +1,49 @@
 import torch
 import time
-from utils.build_matrix import parser
 import pickle
 import sys
 
 
 class ACEnforcer:
-    def __init__(self, cons_map, N, D):
+    def __init__(self, cons_map, n_vars, n_dom):
         self.cons_map = cons_map
-        self.n_mask0 = torch.zeros(N).to(device)
-        self.nnd_mask1 = torch.ones((N, N, D)).to(device)
-        self.nd_mask0 = torch.zeros((N, D)).to(device)
-        self.iteration_count = 0
+        self.n_mask0 = torch.zeros(n_vars).to(device)
+        self.nnd_mask1 = torch.ones((n_vars, n_vars, n_dom)).to(device)
+        self.nd_mask0 = torch.zeros((n_vars, n_dom)).to(device)
+        self.ac_count = 0
 
-    def ac_enforcer(self, vars_map, idx):
-        num_idx = idx.shape[0]
+    def ac_enforcer(self, vars_map, changed_idx):
+        n_idx = changed_idx.shape[0]
         vars_map_pre = vars_map.sum(1)
-        while num_idx != 0:
-            self.iteration_count += 1
+        while n_idx != 0:
+            self.ac_count += 1
 
-            nkd = torch.matmul(self.cons_map[:, idx, :, :], vars_map[idx, :].unsqueeze(2)).squeeze(-1)
+            nkd = torch.matmul(self.cons_map[:, changed_idx, :, :], vars_map[changed_idx, :].unsqueeze(2)).squeeze(-1)
 
-            nd = torch.where(nkd > 1, self.nnd_mask1[:, : num_idx, :], nkd).sum(1)
+            nd = torch.where(nkd > 1, self.nnd_mask1[:, : n_idx, :], nkd).sum(1)
             # nd = (1 - torch.relu(1 - nkd)).sum(1)
 
-            vars_map = torch.where(nd != num_idx, self.nd_mask0, vars_map)
-            # vars_map = torch.relu(nd - num_idx + 1) * vars_map
-            # vars_map = torch.div(nd, num_idx, rounding_mode='floor') * vars_map
+            vars_map = torch.where(nd != n_idx, self.nd_mask0, vars_map)
+            # vars_map = torch.relu(nd - n_idx + 1) * vars_map
+            # vars_map = torch.div(nd, n_idx, rounding_mode='floor') * vars_map
 
             vars_map_sum = vars_map.sum(1)
             if (vars_map_sum == self.n_mask0).any():
                 return None
-            idx = (vars_map_sum != vars_map_pre).nonzero(as_tuple=True)[0]
-            num_idx = idx.shape[0]
+            changed_idx = (vars_map_sum != vars_map_pre).nonzero(as_tuple=True)[0]
+            n_idx = changed_idx.shape[0]
             vars_map_pre = vars_map_sum
 
         return vars_map
 
 
 class BackTrackSearcher:
-    def __init__(self, rel_, N, D):
-        self.acer = ACEnforcer(rel_, N, D)
-        self.D = D
-        self.N = N
-        self.assign_mask = torch.eye(N).to(device)
-        self.n_mask10000 = (torch.ones(N) * 10000).to(device)
+    def __init__(self, rel_, n_vars, n_dom):
+        self.acer = ACEnforcer(rel_, n_vars, n_dom)
+        self.n_dom = n_dom
+        self.n_vars = n_vars
+        self.assign_mask = torch.eye(n_vars).to(device)
+        self.n_mask10000 = (torch.ones(n_vars) * 10000).to(device)
         self.count = 0
         self.answer = None
 
@@ -56,40 +55,40 @@ class BackTrackSearcher:
         vars_re[var_index][val_index] = 1
         return vars_re
 
-    def var_heuristics(self, vars_):
-        dom = vars_.sum(1)
-        dom = torch.where(dom == 1, self.n_mask10000, dom)
-        min_index = dom.argmin().item()
-        if dom[min_index].item() >= 10000:
+    def var_heuristics(self, vars_map):
+        vars_size = vars_map.sum(1)
+        vars_size = torch.where(vars_size == 1, self.n_mask10000, vars_size)
+        min_idx = vars_size.argmin().item()
+        if vars_size[min_idx].item() >= 10000:
             return -1
-        return min_index
+        return min_idx
 
-    def dfs(self, level, vars_pre):
+    def dfs(self, level, vars_map):
         self.count += 1
         if self.count % 100 == 0:
             print(level, self.count)
             if self.count >= cutoff:
                 return True
-        if level == self.N:
-            self.answer = vars_pre
+        if level == self.n_vars:
+            self.answer = vars_map
             return True
 
-        if vars_pre is None:
+        if vars_map is None:
             return False
 
-        # var_index = self.var_heuristics(vars_pre)
-        var_index = level
-        if var_index == -1:
-            self.answer = vars_pre
+        # var_idx = self.var_heuristics(vars_map)
+        var_idx = level
+        if var_idx == -1:
+            self.answer = vars_map
             return True
 
-        var = vars_pre[var_index]
-        for i in range(self.D):
+        var = vars_map[var_idx]
+        for i in range(self.n_dom):
             if var[i] == 0:
                 continue
-            vars_re = self.assignment(var_index, i, vars_pre)
-            vars_re = self.acer.ac_enforcer(vars_re, idx=torch.tensor([var_index]))
-            if self.dfs(level + 1, vars_re):
+            _vars_map = self.assignment(var_idx, i, vars_map)
+            _vars_map = self.acer.ac_enforcer(_vars_map, changed_idx=torch.tensor([var_idx]))
+            if self.dfs(level + 1, _vars_map):
                 return True
 
         return False
@@ -131,11 +130,11 @@ ticks = time.time()
 #     print(bs.answer.squeeze())
 # else:
 #     print("no answer...")
-variables_map = bs.acer.ac_enforcer(variables_map, idx=torch.tensor([i for i in range(num_variables)]))
+variables_map = bs.acer.ac_enforcer(variables_map, changed_idx=torch.tensor([i for i in range(num_variables)]))
 satisfied = bs.dfs(0, variables_map)
 print("Lasts =", time.time() - ticks)
 print(bs.count)
-print(bs.acer.iteration_count / bs.count)
+print(bs.acer.ac_count / bs.count)
 print(satisfied)
 
 # print("Node =", bs.count)
