746e29bd4974eeabb8f7be6b7ae649905534d420
qslim <mqyang1s@163.com>
2022-09-01 15:25:53 +0800

    jax

--- matrix_main.py
+++ matrix_main.py
@@ -1,4 +1,5 @@
-import torch
+import jax.numpy as jnp
+import jax
 import time
 import pickle
 import sys
@@ -9,17 +10,12 @@ import random
 class ACEnforcer:
     def __init__(self, cons_map, n_vars, n_dom):
         # self.cons_map = cons_map
-        self.n_mask0 = torch.zeros(n_vars).to(device)
-        self.nnd_mask1 = torch.ones((n_vars, n_vars, n_dom)).to(device)
-        self.nd_mask0 = torch.zeros((n_vars, n_dom)).to(device)
+        self.n_mask0 = jnp.zeros(n_vars)
+        self.nnd_mask1 = jnp.ones((n_vars, n_vars, n_dom))
+        self.nd_mask0 = jnp.zeros((n_vars, n_dom))
         self.ac_count = 0
 
-        self.cons_map = torch.ones((n_vars, n_vars, n_dom, n_dom)).to(device)
-        for i in range(n_vars):
-            for j in range(i + 1, n_vars):
-                if random.randint(0, 50) == 0:
-                    self.cons_map[i][j] = cons_map[i][j]
-                    self.cons_map[j][i] = cons_map[j][i]
+        self.cons_map = cons_map
 
     def ac_enforcer(self, vars_map, changed_idx):
         n_idx = changed_idx.shape[0]
@@ -27,19 +23,16 @@ class ACEnforcer:
         while n_idx != 0:
             self.ac_count += 1
 
-            nkd = torch.matmul(self.cons_map[:, changed_idx, :, :], vars_map[changed_idx, :].unsqueeze(2)).squeeze(-1)
+            nkd = jnp.matmul(self.cons_map[:, changed_idx, :, :], jnp.expand_dims(vars_map[changed_idx, :], axis=2)).squeeze(-1)
 
-            nd = torch.where(nkd > 1, self.nnd_mask1[:, : n_idx, :], nkd).sum(1)
-            # nd = (1 - torch.relu(1 - nkd)).sum(1)
+            nd = jnp.where(nkd > 1, self.nnd_mask1[:, : n_idx, :], nkd).sum(1)
 
-            vars_map = torch.where(nd != n_idx, self.nd_mask0, vars_map)
-            # vars_map = torch.relu(nd - n_idx + 1) * vars_map
-            # vars_map = torch.div(nd, n_idx, rounding_mode='floor') * vars_map
+            vars_map = jnp.where(nd != n_idx, self.nd_mask0, vars_map)
 
             vars_map_sum = vars_map.sum(1)
             if (vars_map_sum == self.n_mask0).any():
                 return None
-            changed_idx = (vars_map_sum != vars_map_pre).nonzero(as_tuple=True)[0]
+            changed_idx = (vars_map_sum != vars_map_pre).nonzero()[0]
             n_idx = changed_idx.shape[0]
             vars_map_pre = vars_map_sum
 
@@ -51,22 +44,22 @@ class BackTrackSearcher:
         self.acer = ACEnforcer(rel_, n_vars, n_dom)
         self.n_dom = n_dom
         self.n_vars = n_vars
-        self.assign_mask = torch.eye(n_vars).to(device)
-        self.n_mask10000 = (torch.ones(n_vars) * 10000).to(device)
+        self.assign_mask = jnp.eye(n_vars)
+        self.n_mask10000 = (jnp.ones(n_vars) * 10000)
         self.count = 0
         self.answer = None
 
     def assignment(self, var_index, val_index, vars_pre):
         # self.count += 1
-        self.assign_mask[var_index][var_index] = 0
-        vars_re = torch.matmul(self.assign_mask, vars_pre)
-        self.assign_mask[var_index][var_index] = 1
-        vars_re[var_index][val_index] = 1
+        self.assign_mask.at[var_index, var_index].set(0)
+        vars_re = jnp.matmul(self.assign_mask, vars_pre)
+        self.assign_mask.at[var_index, var_index].set(1)
+        vars_re.at[var_index, val_index].set(1)
         return vars_re
 
     def var_heuristics(self, vars_map):
         vars_size = vars_map.sum(1)
-        vars_size = torch.where(vars_size == 1, self.n_mask10000, vars_size)
+        vars_size = jnp.where(vars_size == 1, self.n_mask10000, vars_size)
         min_idx = vars_size.argmin().item()
         if vars_size[min_idx].item() >= 10000:
             return -1
@@ -96,7 +89,7 @@ class BackTrackSearcher:
             if var[i] == 0:
                 continue
             _vars_map = self.assignment(var_idx, i, vars_map)
-            _vars_map = self.acer.ac_enforcer(_vars_map, changed_idx=torch.tensor([var_idx]))
+            _vars_map = self.acer.ac_enforcer(_vars_map, changed_idx=jnp.array([var_idx]))
             if self.dfs(level + 1, _vars_map):
                 return True
 
@@ -105,11 +98,11 @@ class BackTrackSearcher:
 
 random.seed(0)
 chosen_device = 'cuda'
-device = torch.device(chosen_device)
+device = jax.default_device(chosen_device)
 bm_name = None
 cutoff = -1
 bm_cut = [
-    ('sparsedom10-var500-den1-seed0-ts1661767686.dump', 5000)
+    ('dom10-var500-den10-ts1661607900.dump', 5000)
 ]
 csvheader = ['name', 'duration', 'count', 'ac_per', 'satisfied']
 with open('cuda_results.csv', 'w', encoding='UTF8', newline='') as mycsv:
@@ -126,7 +119,7 @@ with open('cuda_results.csv', 'w', encoding='UTF8', newline='') as mycsv:
         num_variables = len(constraints_map)
         f.close()
 
-        constraints_map = torch.tensor(constraints_map).type(torch.float)
+        constraints_map = jnp.array(constraints_map)
 
         # build vars_map
         variables_map = []
@@ -135,13 +128,13 @@ with open('cuda_results.csv', 'w', encoding='UTF8', newline='') as mycsv:
             for _ in range(max_domain):
                 line.append(1)
             variables_map.append(line)
-        variables_map = torch.tensor(variables_map).type(torch.float)
+        variables_map = jnp.array(variables_map)
 
         print("cons shape:", constraints_map.shape, " vars shape:", variables_map.shape)
-        print(constraints_map.type(), " ", variables_map.type())
+        # print(constraints_map.type(), " ", variables_map.type())
 
-        variables_map = variables_map.to(device)
-        constraints_map = constraints_map.to(device)
+        # variables_map = variables_map.to(device)
+        # constraints_map = constraints_map.to(device)
 
         bs = BackTrackSearcher(constraints_map, num_variables, max_domain)
 
@@ -152,7 +145,7 @@ with open('cuda_results.csv', 'w', encoding='UTF8', newline='') as mycsv:
         #     print(bs.answer.squeeze())
         # else:
         #     print("no answer...")
-        variables_map = bs.acer.ac_enforcer(variables_map, changed_idx=torch.tensor([i for i in range(num_variables)]))
+        variables_map = bs.acer.ac_enforcer(variables_map, changed_idx=jnp.array([i for i in range(num_variables)]))
         satisfied = bs.dfs(0, variables_map)
         duration = time.time() - ticks
         count = bs.count
